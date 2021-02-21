cd208a533230f1c74c19ae7661dc6f2baed52e9e
AdeomoAde <53475990+niyiade97@users.noreply.github.com>
2021-02-21 11:36:44 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+
+var currentStatus = false;
+
+    document.getElementById("menu-icon").addEventListener("click",function(){
+        dropMenu(currentStatus);
+      });
+     
+     
+
+      function dropMenu(status){
+        if(status){
+            currentStatus = false;
+            document.getElementById("menu").removeAttribute("class");  
+              document.getElementById("menu").setAttribute("class", "fa fa-bars");
+            document.getElementById("nav-links").style.display = "none";
+
+        }
+        else{
+            currentStatus = true;
+            removeCurrentStatus =true;
+            document.getElementById("menu").removeAttribute("class");
+            document.getElementById("menu").setAttribute("class", "fa fa-close");
+            document.getElementById("nav-links").style.display = "block";
+            
+
+        }
+      }
+     
+      window.addEventListener("resize", function(){
+          if( window.innerWidth <= 600){  
+            document.getElementById("menu").removeAttribute("class");  
+              document.getElementById("menu").setAttribute("class", "fa fa-bars");
+              document.getElementById("nav-links").style.display = "none";
+          }
+          else{
+            document.getElementById("menu").removeAttribute("class");
+            document.getElementById("nav-links").style.display = "block";
+            
+            
+          }
+      });
+      window.addEventListener("reload", function(){
+        if(window.innerWidth <= 600){
+            document.getElementById("menu").setAttribute("class", "fa fa-bars");
+        }
+
+      });
+
+     
